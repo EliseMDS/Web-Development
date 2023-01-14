67f61725b50e9152c3d84eb73d6c85ca20a14b60
EliseMDS <elise.mdossantos@gmail.com>
2023-01-14 14:33:02 +0100

    Style the keeper app

--- my-app/src/components/InputArea.jsx
+++ my-app/src/components/InputArea.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react"
+import AddIcon from '@mui/icons-material/Add';
+import { Fab, Zoom  } from '@mui/material';
 
 export default function InputArea(props){
+
+    const [isExpanded, setExpanded] = useState(false);
 
     const [inputNote, setNote] = useState({
         title: "",
@@ -16,17 +20,23 @@ export default function InputArea(props){
         }))
     }
 
+    function expand() {
+        setExpanded((prevValue) => !prevValue)
+    }
+
     return(
-        <form onSubmit={(event) => event.preventDefault()}>
-            <input onChange={handleChange} type="text" name="title" value={inputNote.title} placeholder="Title"/>
-            <textarea onChange={handleChange} name="content" value={inputNote.content} placeholder="Take a note..."/>
-            <button 
-                onClick={() => {
-                    props.onClick(inputNote)
-                    setNote({title: "", content:""})
-                }} 
-                type="submit">Add
-            </button>
+        <form className="create-note" onSubmit={(event) => event.preventDefault()}>
+            {isExpanded && <input onChange={handleChange} type="text" name="title" value={inputNote.title} placeholder="Title"/>}            
+            <textarea onClick={expand} onChange={handleChange} name="content" value={inputNote.content} placeholder="Take a note..." rows={isExpanded ? 3 : 1}/>
+            <Zoom in={isExpanded}>
+                <Fab 
+                    onClick={() => {
+                        props.onClick(inputNote)
+                        setNote({title: "", content:""})
+                    }} 
+                    type="submit"><AddIcon />
+                </Fab>
+            </Zoom>
         </form>
     )
 }
